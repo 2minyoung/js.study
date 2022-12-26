fcf1beb169f2bb3871709f3c516e97d64ccb94f0
qkqh1577 <lmy-@nate.com>
2022-12-26 18:09:48 +0900

    20221226 색션 1 완료 2 진행중

---
+++ 강의 자료/section2/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,10 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+currentResult = currentResult + 10 * 3;
+
+let calculationDescription = `(${defaultResult} + 10) * 3`;
+// let errorMessage = 'An error \n' + 'occurred!';
+
+outputResult(currentResult, calculationDescription);
