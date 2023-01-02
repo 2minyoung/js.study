f8bad749ca96b08714bf285e36c5a34ac189dd27
qkqh1577 <lmy-@nate.com>
2023-01-02 17:10:26 +0900

    20230102 색션 3 완료 4 진행중

--- 강의 자료/section2/basics-01-starting-project/assets/scripts/app.js
+++ 강의 자료/section2/basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,85 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
+// Gets input from input field
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
 
-currentResult = currentResult + 10 * 3;
+// Generates and writes calautation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
 
-let calculationDescription = `(${defaultResult} + 10) * 3`;
-// let errorMessage = 'An error \n' + 'occurred!';
+function writeToLog(
+  operationIdentifier,
+  perResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    pervResult: perResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
 
-outputResult(currentResult, calculationDescription);
+function calculaterResult(calculationType) {
+  const enteredNumber = getUserNumberInput();
+  if (
+    calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' &&
+    calculationType !== 'DIVIDE' ||
+    !enteredNumber
+  ) {
+    return;
+  }
+
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === "ADD") {
+    currentResult += enteredNumber;
+    mathOperator = "+";
+  } else if (calculationType === 'SUBTRACT') {
+    currentResult -= enteredNumber;
+    mathOperator = "-";
+  } else if (calculationType === 'MULTIPLY') {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (calculationType === 'DIVIDE') {
+    currentResult /= enteredNumber;
+    mathOperator = "/";
+  }
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+
+function add() {
+  calculaterResult("ADD");
+}
+
+function subtract() {
+  calculaterResult("SUBTRACT");
+}
+
+function multiply() {
+  calculaterResult("MULTIPLY");
+}
+
+function divide() {
+  calculaterResult("DIVIDE");
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+
+// 파이썬 f 스트링 같은거임
+// let calculationDescription = `(${defaultResult} + 10) * 3`;
