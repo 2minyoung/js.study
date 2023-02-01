2a666ccf8b5356788187ccf26327a4c8bbc1b9cc
qkqh1577 <lmy-@nate.com>
2023-02-01 18:02:23 +0900

    20230201 merge

--- 강의 자료/section17/async-01-starting-setup/app.js
+++ 강의 자료/section17/async-01-starting-setup/app.js
@@ -1,19 +1,73 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
-function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    (posData) => {
-      console.log(posData);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
-  console.log('Getting position...')
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let tiemrData;
+  try {
+    posData = await getPosition();
+    tiemrData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(tiemrData, posData);
+  // getPosition()
+  //   .then((posData) => {
+  //     positionData = posData;
+  //     return setTimer(2000);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     return 'on we go...'
+  //   })
+  //   .then((data) => {
+  //     console.log(data, positionData);
+  //   });
+
+  // setTimer(1000).then(() => {
+  //   console.log("Timer done!");
+  // });
+  // console.log("Getting position...");
 }
 
 button.addEventListener("click", trackUserHandler);
+
+// Promise.race([getPosition(), setTimer(1000)]).then((data) => {
+//   console.log(data);
+// });
+
+// Promise.all([getPosition(), setTimer(1000)]).then((promiseData) => {
+//   console.log(promiseData);
+// });
+
+Promise.allSettled([getPosition(), setTimer(1000)]).then((promiseData) => {
+  console.log(promiseData);
+});
 
 // let result = 0;
 
